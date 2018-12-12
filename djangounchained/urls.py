f7b3426dc6a57a8d606d957c19c99b3c168b5b93
Sazzad <sazzad.rahman.bd@gmail.com>
2018-12-12 09:36:04 +0600

    Blog app

--- djangounchained/urls.py
+++ djangounchained/urls.py
@@ -20,9 +20,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.default),
+    # path('', views.default),
+    path('', include('blog.urls')),
 
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
