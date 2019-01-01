4ce35ba5b7d0ed58b881d27cd446bb65f6b9bd1f
Sazzad <sazzad.rahman.bd@gmail.com>
2019-01-01 10:52:46 +0600

    Optimize profile pic

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -13,3 +14,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return '{} Profile'.format(self.user)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        super().save(force_insert, force_update, using, update_fields)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
